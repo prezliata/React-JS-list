21dfd09cea15c07190fc7b5d740d3815375944d3
Taras <taras.prezlyata.sign.in@gmail.com>
2019-10-25 00:07:12 +0300

    added new features

--- src/App.js
+++ src/App.js
@@ -1,63 +1,114 @@
-import React, {Component} from 'react';
-import NavBar from './components/NavBar'
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
 import Counters from './components/Counters';
 
 import './App.css';
 
 class App extends Component {
-  state = {
-    counters: [
-       {id:1, value: 0},
-       {id:2, value: 0},
-       {id:3, value: 0},
-       {id:4, value: 4}
-    ]
-};
+	state = {
+		name: '',
+		value: 0,
+		counters: [
+			{ id: 1, name: 'One', value: 0 },
+			{ id: 2, name: 'One', value: 0 },
+			{ id: 3, name: 'One', value: 0 },
+			{ id: 4, name: 'One', value: 4 }
+		]
+	};
 
-handleReset = () => {
-    const counters = this.state.counters.map(c =>{
-        c.value = 0;
-        return c;
-    });
-    this.setState({ counters });
-};
+	// componentDidMount() {
+	// 	this.addItem();
+	// 	console.log(this.state);
+	// }
+	onChangeName = (e) => {
+		let name = e.target.value === '' ? 0 : e.target.value;
+		this.setState({
+			name: name
+		});
+	};
 
-handleIncrement = counter =>  {
-   const counters = [...this.state.counters];
-   const index = counters.indexOf(counter);
-   counters[index] = {...counter};
-   counters[index].value++;
-   this.setState({counters})
-}
-handleDecrement = counter =>  {
-  const counters = [...this.state.counters];
-  const index = counters.indexOf(counter);
-  counters[index] = {...counter};
-  counters[index].value--;
-  this.setState({counters})
-}
+	onChangeValue = (e) => {
+		let valueNumber = e.target.value;
+		this.setState({
+			value: valueNumber
+		});
+	};
 
-handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(c => c.id !== counterId);
-    this.setState({counters});
-};
+	addItem = () => {
+		let newItem = {
+			id: this.state.counters.length + 1,
+			name: this.state.name,
+			value: this.state.value
+		};
+		let newCountersArr = this.state.counters;
+		newCountersArr.push(newItem);
 
-  render(){
-    return(
-      <React.Fragment>
-      <NavBar totalCounters={this.state.counters.filter(c => c.value>0).length}/>
-      <main className="container">
-        <Counters 
-          counters = {this.state.counters}
-          onReset={this.handleReset} 
-          onIncrement = {this.handleIncrement}
-          onDecrement = {this.handleDecrement}  
-          onDelete ={this.handleDelete} 
-        />
-      </main>
-      </React.Fragment>
-    );
-  }
+		this.setState({
+			counters: newCountersArr
+		});
+		this.reset();
+	};
+
+	reset() {
+		this.setState({
+			name: '',
+			value: 0
+		});
+		this.myFormRef.reset();
+	}
+
+	handleReset = () => {
+		const counters = this.state.counters.map((c) => {
+			c.value = 0;
+			return c;
+		});
+		this.setState({ counters });
+	};
+
+	handleIncrement = (counter) => {
+		const counters = [ ...this.state.counters ];
+		const index = counters.indexOf(counter);
+		counters[index] = { ...counter };
+		counters[index].value++;
+		this.setState({ counters });
+	};
+	handleDecrement = (counter) => {
+		const counters = [ ...this.state.counters ];
+		const index = counters.indexOf(counter);
+		counters[index] = { ...counter };
+		counters[index].value--;
+		this.setState({ counters });
+	};
+
+	handleDelete = (counterId) => {
+		const counters = this.state.counters.filter((c) => c.id !== counterId);
+		this.setState({ counters });
+	};
+
+	render() {
+		console.log(this.state.counters);
+		return (
+			<React.Fragment>
+				<NavBar totalCounters={this.state.counters.filter((c) => c.value > 0).length} />
+				<main className="container">
+					<form ref={(el) => (this.myFormRef = el)}>
+						<input onChange={(e) => this.onChangeName(e)} />
+						<input onChange={(e) => this.onChangeValue(e)} />
+					</form>
+					<button onClick={() => this.addItem()} className="btn btn-primary btn-sm">
+						Add item
+					</button>
+					<Counters
+						counters={this.state.counters}
+						onReset={this.handleReset}
+						onIncrement={this.handleIncrement}
+						onDecrement={this.handleDecrement}
+						onDelete={this.handleDelete}
+					/>
+				</main>
+			</React.Fragment>
+		);
+	}
 }
 
 export default App;
--- src/components/Counters.js
+++ src/components/Counters.js
@@ -2,34 +2,31 @@ import React, { Component } from 'react';
 import Counter from './Counter';
 
 class Counters extends Component {
+	componentDidUpdate(prevProps, prevState) {
+		// console.log('prevProps', prevProps);
+		// console.log('prevState', prevState);
+	}
 
-    componentDidUpdate(prevProps, prevState){
-        console.log('prevProps', prevProps);
-        console.log('prevState', prevState);
-    }
+	render() {
+		const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props;
 
-    render(){
-
-        const {onReset, counters, onDelete, onIncrement, onDecrement} = this.props;
-
-        return(
-            <div>
-                <button
-                    onClick = {onReset}
-                    className="btn btn-primary btn-sm m-2"
-                >Reset</button>
-                {counters.map(counter => (
-                    <Counter 
-                        key={counter.id} 
-                        onDelete={onDelete}
-                        onIncrement = {onIncrement} 
-                        counter = {counter}
-                        onDecrement = {onDecrement}
-                    />
-                ))}
-            </div>
-        );
-    }
+		return (
+			<div>
+				<button onClick={onReset} className="btn btn-primary btn-sm m-2">
+					Reset
+				</button>
+				{counters.map((counter) => (
+					<Counter
+						key={counter.id}
+						onDelete={onDelete}
+						onIncrement={onIncrement}
+						counter={counter}
+						onDecrement={onDecrement}
+					/>
+				))}
+			</div>
+		);
+	}
 }
 
 export default Counters;
